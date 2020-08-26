660690bd3033a35d2bdcc409b33a9effeeb9e0f4
luojunbin <luojunbin@bytedance.com>
2020-08-26 11:17:13 +0800

    feat(server): server 修改依赖

--- packages/server/src/sharedInternals.js
+++ packages/server/src/sharedInternals.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from '@loadable/component'
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from '@byted/loadable-component-wip'
 
 const {
   invariant,
